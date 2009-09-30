4fa8320fc95b0b478ad2bedfeaad7acb9f147c17
Sebastian Majstorovic <canbuffi@googlemail.com>
2009-09-30 08:54:30 +0200

    added ip geocoding and gears support (v0.5)

--- jquery.geolocation.js
+++ jquery.geolocation.js
@@ -1,39 +1,68 @@
 /**
- * A jQuery-Geolocation-Plugin
+ * An extended jQuery-Geolocation-Plugin
+ * 
  *
- * @author Thomas Michelbach <thomas@nomoresleep.net>
- * @copyright NoMoreSleep(tm) <http://developer.nomoresleep.net>
- * @version 0.1
+ * @author Sebastian Majstorovic <canbuffi@gmail.com>
+ * @initial_author Thomas Michelbach <thomas@nomoresleep.net>
+ * @copyright majstorovic & majstorovic gbr <http://www.canbuffi.de>
+ * @version 0.5
  * @license MIT LICENSE <http://www.opensource.org/licenses/mit-license.php>
  *
  */
 
 (function($){
-	
 	$.extend($.support,{
 		geolocation:function(){
 			return $.geolocation.support();
 		}
 	});
 
-	$.geolocation = {        
-		find:function(success, error, options){
-			if($.geolocation.support()){
-				options = $.extend({highAccuracy: false, track: false}, options);
-				($.geolocation.object())[(options.track ? 'watchPosition' : 'getCurrentPosition')](function(location){
-					success(location.coords);
-				}, function(){
+	$.geolocation = {
+		browser: {
+			find:function(success, error, options){
+				if($.geolocation.browser.support()){
+					options = $.extend({highAccuracy: false, track: false}, options);
+					($.geolocation.browser.object())[(options.track ? 'watchPosition' : 'getCurrentPosition')](function(location){
+						success(location.coords.latitude, location.coords.longitude);
+					}, function(locationError){
+						error(locationError);
+					}, {enableHighAccuracy: options.highAccuracy, timeout: options.timeout, maximumAge: options.maximumAge});		
+				}else{
 					error();
-				}, {enableHighAccuracy: options.highAccuracy});		
-			}else{
-				error();				
+				}
+			},
+			object:function(){
+				if (google.gears) {
+					return google.gears.factory.create('beta.geolocation');
+				}
+				return navigator.geolocation;
+			},
+			support:function(){
+				return ($.geolocation.browser.object()) ? true : false;
 			}
 		},
-		object:function(){
-			return navigator.geolocation;
+		ip: {
+			find:function(success, error){
+				if ($.geolocation.ip.support()){
+					success($.geolocation.ip.object().latitude, $.geolocation.ip.object().longitude);
+				} else {
+					error();
+				}
+			},
+			object:function(){
+				return google.loader.ClientLocation;
+			},
+			support:function(){
+				return (google && google.loader && google.loader.ClientLocation) ? true : false;
+			}
 		},
-		support:function(){
-			return ($.geolocation.object()) ? true : false;
+		find:function(success, error, options){
+			$.geolocation.browser.find(success, function(locationError){
+				$.geolocation.ip.find(success, error);
+			}, options);
+		},
+		support: function(){
+			return ($.geolocation.browser.support() || $.geolocation.ip.support()) ? true : false;
 		}
 	};
 	
